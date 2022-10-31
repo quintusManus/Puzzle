3a8c5e2165d54152dc88fcab9fa3e01476e2ca3c
sdbridges <sdbridges@github.com>
2022-10-31 02:12:12 +0100

    Update pom file to point to main class Puzzle Application, add JPA dependency,  create templates folder for thymeleaf views, add h2 database for temporary database

--- puzzle/src/main/java/csc340/caffeinatedfoxes/puzzle/PuzzleApplication.java
+++ puzzle/src/main/java/csc340/caffeinatedfoxes/puzzle/PuzzleApplication.java
@@ -1,12 +1,15 @@
 package csc340.caffeinatedfoxes.puzzle;
 
 import org.springframework.boot.SpringApplication;
+
+//attempts to automatically configure your Spring application based on the jar dependencies that you have added (pom)
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PuzzleApplication {
 
 	public static void main(String[] args) {
+//              Bootstraps a spring application as a stand-alone application using PuzzleApplication  class as the entry point
 		SpringApplication.run(PuzzleApplication.class, args);
 	}
 
--- puzzle/src/main/java/csc340/caffeinatedfoxes/puzzle/PuzzleController.java
+++ puzzle/src/main/java/csc340/caffeinatedfoxes/puzzle/PuzzleController.java
@@ -1,10 +1,22 @@
 
 package csc340.caffeinatedfoxes.puzzle;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  *
  * @author sofie
  */
+
+//The @Controller annotation is a specialization of the generic stereotype @Component annotation, which allows a class to be recognized as a Spring-managed component.
+@Controller
 public class PuzzleController {
-    
+        // handler method to handle home page request
+    @GetMapping("/")
+    public String showHome(Model model){
+        return "index";
+    }
+}
 }
