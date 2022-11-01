430e6d3824b0c559a118a379322d038308e01ebc
Benjamin Woods <benewoods02@gmail.com>
2022-11-01 14:07:34 -0400

    working on User class

---
+++ puzzle/src/main/java/csc340/caffeinatedfoxes/puzzle/User.java
@@ -0,0 +1,51 @@
+package csc340.caffeinatedfoxes.puzzle;
+
+public class User {
+    protected Long id;
+    protected String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    protected String password;
+    protected String type;
+
+    public User(String email, String password, String type) {
+        this.email = email;
+        this.password = password;
+        this.type = type;
+    }
+
+    public void createPassword(){
+        //put this.password into the database
+    }
+}
