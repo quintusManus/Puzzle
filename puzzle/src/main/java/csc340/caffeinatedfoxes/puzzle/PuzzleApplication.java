fc36fe845b9899bb803395334d6fb05f4e05423d
Benjamin Woods <benewoods02@gmail.com>
2022-11-04 21:21:47 -0400

    fixed the unable to access other pages issue

--- puzzle/src/main/java/csc340/caffeinatedfoxes/puzzle/PuzzleApplication.java
+++ puzzle/src/main/java/csc340/caffeinatedfoxes/puzzle/PuzzleApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 //attempts to automatically configure your Spring application based on the jar dependencies that you have added (pom)
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//excluding these two that cause errors related to sql and accessing pages respectively
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class  })
 public class PuzzleApplication {
 
 	public static void main(String[] args) {
