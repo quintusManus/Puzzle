2412cc843b4dba946db7dc5e60ea911ef4a9c0c4
Benjamin Woods <benewoods02@gmail.com>
2022-11-12 20:38:34 -0500

    added new .sql file

--- route.sql
+++ route.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2022 at 09:49 PM
+-- Generation Time: Nov 13, 2022 at 02:38 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -67,10 +67,17 @@ INSERT INTO `route` (`id`, `climbingStyle`, `difficulty`, `locationAndEnvironmen
 CREATE TABLE `users` (
   `id` bigint(20) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `first_name` varchar(20) NOT NULL,
-  `last_name` varchar(20) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `type` varchar(20) NOT NULL,
   `password` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `name`, `type`, `password`) VALUES
+(1, 'benewoods02@gmail.com', 'Ben', 'admin', 'puzzledb');
 
 --
 -- Indexes for dumped tables
@@ -115,7 +122,7 @@ ALTER TABLE `route`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
